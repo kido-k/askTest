c028ccb93daf44be51849892d249c25e8d60b4b5
kotaro_k <kotaro.k13@gmail.com>
2018-12-06 13:56:25 +0900

    modify Logo compornent

--- components/atoms/AskDoctorLogo.vue
+++ components/atoms/AskDoctorLogo.vue
@@ -4,7 +4,6 @@
       <img
         src="../../assets/LOGO.png"
         alt="AskDoctor">
-        <!-- <img src="https://www.askdoctors.jp/assets/green/logo_askdoctors-4374fcc4f8f8724eba9820939b145402bcc88a4042ecc258517299c7c9f41a76.svg" alt="AskDoctor"> -->
     </a>
   </div>
 </template>
@@ -13,6 +12,5 @@
 .Logo {
   display: inline-block;
   height: 42px;
-  /* width: 247.27px; */
 }
 </style>
