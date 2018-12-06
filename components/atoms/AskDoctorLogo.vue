48de33e092d4712f46bdb0b4330b775ca6bfbc95
kotaro_k <kotaro.k13@gmail.com>
2018-12-06 13:49:44 +0900

    add Logo and favicon

---
+++ components/atoms/AskDoctorLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Logo">
+    <a href="/">
+      <img
+        src="../../assets/LOGO.png"
+        alt="AskDoctor">
+        <!-- <img src="https://www.askdoctors.jp/assets/green/logo_askdoctors-4374fcc4f8f8724eba9820939b145402bcc88a4042ecc258517299c7c9f41a76.svg" alt="AskDoctor"> -->
+    </a>
+  </div>
+</template>
+
+<style>
+.Logo {
+  display: inline-block;
+  height: 42px;
+  /* width: 247.27px; */
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
+      <AskDoctor/>
       <h1 class="title">
-        askdoctor
+        hello-project
       </h1>
       <h2 class="subtitle">
-        My wicked Nuxt.js project
+        Nuxt.js project
       </h2>
       <div class="links">
         <a
@@ -23,17 +23,16 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import AskDoctor from '~/components/atoms/AskDoctorLogo.vue'
 
 export default {
   components: {
-    Logo
+    AskDoctor
   }
 }
 </script>
 
 <style>
-
 .container {
   min-height: 100vh;
   display: flex;
@@ -44,7 +43,7 @@ export default {
 
 .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
